ed6ec60951fe2652fac69452dce85e280fcde113
ijknabla <ijknabla@gmail.com>
2025-07-20 00:31:16 +0900

    Implement test for bind

--- src/test/python/test_awaitable.py
+++ src/test/python/test_awaitable.py
@@ -2,7 +2,13 @@ import pytest
 from aiomonad import pure
 
 
+async def async_double(i: int) -> int:
+    return i * 2
+
+
 @pytest.mark.parametrize("i", [-1, 0, +1])
 @pytest.mark.asyncio
 async def test_await_pure(i: int):
     assert await pure(i) == i
+    assert await (pure(i) * async_double) == i * 2
+    assert await (pure(i) * async_double * async_double) == i * 4
