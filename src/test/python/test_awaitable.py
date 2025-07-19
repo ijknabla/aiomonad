4a00cecc8700865e5d89707c075efa18d5085d08
ijknabla <ijknabla@gmail.com>
2025-07-20 00:37:58 +0900

    Implement test for map

--- src/test/python/test_awaitable.py
+++ src/test/python/test_awaitable.py
@@ -2,13 +2,20 @@ import pytest
 from aiomonad import pure
 
 
+def double(i: int) -> int:
+    return i * 2
+
+
 async def async_double(i: int) -> int:
     return i * 2
 
 
 @pytest.mark.parametrize("i", [-1, 0, +1])
 @pytest.mark.asyncio
-async def test_await_pure(i: int):
+async def test_await_pure(i: int) -> None:
     assert await pure(i) == i
     assert await (pure(i) * async_double) == i * 2
     assert await (pure(i) * async_double * async_double) == i * 4
+
+    assert await (pure(i) / double) == i * 2
+    assert await (pure(i) / double / double) == i * 4
